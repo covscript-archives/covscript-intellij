15029136e84666856437c2f54637bad1b3000483
ice1000 <ice1000kotlin@foxmail.com>
2018-01-10 01:23:14 +0800

    [ #7 ] Remove facet type

--- src/org/covscript/lang/module/cov-modules.kt
+++ src/org/covscript/lang/module/cov-modules.kt
@@ -2,8 +2,7 @@ package org.covscript.lang.module
 
 import com.intellij.facet.FacetManager
 import com.intellij.facet.FacetTypeRegistry
-import com.intellij.ide.util.projectWizard.ModuleBuilder
-import com.intellij.ide.util.projectWizard.WizardContext
+import com.intellij.ide.util.projectWizard.*
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.module.*
 import com.intellij.openapi.projectRoots.Sdk
@@ -16,7 +15,11 @@ import com.intellij.openapi.vfs.LocalFileSystem
 import org.covscript.lang.*
 import java.nio.file.*
 
-class CovModuleBuilder : ModuleBuilder() {
+class CovModuleBuilder : ModuleBuilder(), ModuleBuilderListener {
+	init {
+		addListener(this)
+	}
+
 	lateinit var sdk: Sdk
 	private var sourcePaths = mutableListOf<Pair<String, String>>()
 		get() {
@@ -29,7 +32,8 @@ class CovModuleBuilder : ModuleBuilder() {
 		}
 
 	override fun isSuitableSdkType(sdkType: SdkTypeId?) = sdkType is CovSdkType
-	override fun getWeight() = 100
+	override fun getWeight() = 99
+	override fun getNodeIcon() = COV_BIG_ICON
 	override fun getModuleType() = CovModuleType.instance
 	override fun getCustomOptionsStep(context: WizardContext, parentDisposable: Disposable): CovSetupSdkWizardStep {
 		parentDisposable.dispose()
@@ -49,25 +53,14 @@ class CovModuleBuilder : ModuleBuilder() {
 					?: return@forEach
 			contentEntry.addSourceFolder(sourceRoot, false, libInfo.second)
 		}
-		setupFacet(model.module, sdk)
-		ProjectRootManager.getInstance(model.module.project).run {
-			projectSdk = sdk
-		}
 	}
 
-	private fun setupFacet(module: Module, sdk: Sdk) {
-		val facetId = CovFacetType.stringId
-		if (facetId.isNotBlank()) {
-			val facetManager = FacetManager.getInstance(module)
-			val type = FacetTypeRegistry.getInstance().findFacetType(facetId) as? CovFacetType ?: return
-			if (facetManager.getFacetByType(type.id) == null) {
-				val model = facetManager.createModifiableModel()
-				val facet = facetManager.addFacet(type, type.defaultFacetName, null) as? CovFacet ?: return
-				facet.configuration.data.covSdkName = sdk.name
-				model.addFacet(facet)
-				model.commit()
-			}
-		}
+	override fun moduleCreated(module: Module) {
+		ProjectRootManager.getInstance(module.project).projectSdk = sdk
+	}
+
+	override fun modifySettingsStep(settingsStep: SettingsStep): ModuleWizardStep? {
+		return super.modifySettingsStep(settingsStep)
 	}
 }
 
