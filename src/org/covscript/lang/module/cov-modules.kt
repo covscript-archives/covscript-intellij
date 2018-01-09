ff38505c02580a0d1b905d8ccd50b67661bfdf9b
ice1000 <ice1000kotlin@foxmail.com>
2018-01-10 01:04:15 +0800

    [ #7 ] Please work

--- src/org/covscript/lang/module/cov-modules.kt
+++ src/org/covscript/lang/module/cov-modules.kt
@@ -2,39 +2,64 @@ package org.covscript.lang.module
 
 import com.intellij.facet.FacetManager
 import com.intellij.facet.FacetTypeRegistry
-import com.intellij.ide.util.projectWizard.*
+import com.intellij.ide.util.projectWizard.ModuleBuilder
+import com.intellij.ide.util.projectWizard.WizardContext
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.module.*
 import com.intellij.openapi.projectRoots.Sdk
-import com.intellij.openapi.roots.ModifiableRootModel
+import com.intellij.openapi.projectRoots.SdkTypeId
+import com.intellij.openapi.roots.*
 import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.Pair
+import com.intellij.openapi.util.io.FileUtil
+import com.intellij.openapi.vfs.LocalFileSystem
 import org.covscript.lang.*
 import java.nio.file.*
 
-
-class CovModuleBuilder : ModuleBuilder(), ModuleBuilderListener {
+class CovModuleBuilder : ModuleBuilder() {
 	lateinit var sdk: Sdk
+	private var sourcePaths = mutableListOf<Pair<String, String>>()
+		get() {
+			if (field.isEmpty()) {
+				val path = Paths.get(contentEntryPath, "src")
+				Files.createDirectories(path)
+				field.add(Pair.create(path.toAbsolutePath().toString(), ""))
+			}
+			return field
+		}
+
+	override fun isSuitableSdkType(sdkType: SdkTypeId?) = sdkType is CovSdkType
+	override fun getWeight() = 100
 	override fun getModuleType() = CovModuleType.instance
 	override fun getCustomOptionsStep(context: WizardContext, parentDisposable: Disposable): CovSetupSdkWizardStep {
 		parentDisposable.dispose()
 		context.projectName = COV_DEFAULT_MODULE_NAME
 		return CovSetupSdkWizardStep(this)
 	}
-	override fun setupRootModel(rootModel: ModifiableRootModel) {
-	}
 
-	override fun moduleCreated(module: Module) {
-		setupFacet(module, sdk)
-		// ModuleRootManager.getInstance(module).sourceRoots.forEach {
-		//   do init
-		// }
+	override fun setupRootModel(model: ModifiableRootModel) {
+		model.getModuleExtension(CompilerModuleExtension::class.java)?.isExcludeOutput = true
+		model.sdk = sdk
+		val contentEntry = doAddContentEntry(model) ?: return
+		sourcePaths.forEach { libInfo ->
+			Files.createDirectories(Paths.get(libInfo.first))
+			val sourceRoot = LocalFileSystem
+					.getInstance().
+					refreshAndFindFileByPath(FileUtil.toSystemIndependentName(libInfo.first))
+					?: return@forEach
+			contentEntry.addSourceFolder(sourceRoot, false, libInfo.second)
+		}
+		setupFacet(model.module, sdk)
+		ProjectRootManager.getInstance(model.module.project).run {
+			projectSdk = sdk
+		}
 	}
 
 	private fun setupFacet(module: Module, sdk: Sdk) {
 		val facetId = CovFacetType.stringId
 		if (facetId.isNotBlank()) {
 			val facetManager = FacetManager.getInstance(module)
-			val type = FacetTypeRegistry.getInstance().findFacetType(facetId) ?: return
+			val type = FacetTypeRegistry.getInstance().findFacetType(facetId) as? CovFacetType ?: return
 			if (facetManager.getFacetByType(type.id) == null) {
 				val model = facetManager.createModifiableModel()
 				val facet = facetManager.addFacet(type, type.defaultFacetName, null) as? CovFacet ?: return
