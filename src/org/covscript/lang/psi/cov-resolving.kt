858eae900d1610c7c23fbd240d262e60583918f5
ice1000 <ice1000kotlin@foxmail.com>
2018-01-20 11:27:22 +0800

    [ resolving ] Try implementing resolving

--- src/org/covscript/lang/psi/cov-resolving.kt
+++ src/org/covscript/lang/psi/cov-resolving.kt
@@ -1,35 +1,60 @@
 package org.covscript.lang.psi
 
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.TextRange
 import com.intellij.psi.*
 import com.intellij.psi.impl.source.resolve.ResolveCache
+import com.intellij.psi.scope.PsiScopeProcessor
+import com.intellij.psi.util.PsiTreeUtil
 
 class CovSymbolRef(symbol: CovSymbol, private var refTo: CovVariableDeclaration? = null) :
-		PsiPolyVariantReferenceBase<CovSymbol>(symbol, symbol.textRange, true) {
+		PsiPolyVariantReferenceBase<CovSymbol>(symbol, TextRange(0, symbol.textLength), true) {
 	private val project = symbol.project
 	override fun equals(other: Any?) = (other as? CovSymbolRef)?.element == element
 	override fun hashCode() = element.hashCode()
+	override fun getCanonicalText(): String = element.text
 	override fun getVariants(): Array<Any> = arrayOf()
-	override fun isReferenceTo(o: PsiElement?) = (o as? CovVariableDeclaration)?.symbol?.text == element.text
+	override fun isReferenceTo(o: PsiElement?) = o === refTo || (o as? CovVariableDeclaration)?.symbol?.text == element.text
 	override fun resolve() = refTo ?: super.resolve().let { it as? CovVariableDeclaration }?.also { refTo = it }
 	override fun multiResolve(incompleteCode: Boolean): Array<ResolveResult> = ResolveCache
 			.getInstance(project)
 			.resolveWithCaching(this, Resolver, true, incompleteCode)
 
 	private companion object Resolver : ResolveCache.PolyVariantResolver<CovSymbolRef> {
-		override fun resolve(ref: CovSymbolRef, incompleteCode: Boolean): Array<ResolveResult> {
+		override fun resolve(ref: CovSymbolRef, incompleteCode: Boolean): Array<out ResolveResult> {
 			val currentSymbol = ref.element ?: return emptyArray()
 			if (currentSymbol.parent !is CovSuffixedExpression) return emptyArray()
-			var currentLookingScope = ref.element.parent
-			val ret = arrayListOf<PsiElementResolveResult>()
-			while (currentLookingScope != null) {
-				val candidate = currentLookingScope
-						.children
-						.mapNotNull { (it as? CovVariableDeclaration)?.symbol }
-						.firstOrNull { it.text == currentSymbol.text }
-				candidate?.let(::PsiElementResolveResult)?.let(ret::add)
-				currentLookingScope = currentLookingScope.parent
-			}
-			return ret.toTypedArray()
+			val processor = SymbolResolveProcessor(ref, incompleteCode)
+			val statement = PsiTreeUtil.getParentOfType(currentSymbol, CovStatement::class.java) ?: return emptyArray()
+			statement.processDeclarations(processor, ResolveState.initial(), currentSymbol, processor.place)
+			return processor.candidates
 		}
 	}
+}
+
+abstract class ResolveProcessor(val name: String) : PsiScopeProcessor {
+	var candidateSet: MutableSet<PsiElementResolveResult> = HashSet()
+	val elements get() = candidateSet.map { it.element }
+	val candidates get() = candidateSet.toTypedArray()
+	override fun handleEvent(event: PsiScopeProcessor.Event, o: Any?) = Unit
+	fun hasCandidates() = candidateSet.isNotEmpty()
+	fun addCandidate(candidate: PsiElementResolveResult) = candidateSet.add(candidate)
+}
+
+class SymbolResolveProcessor(name: String, val place: PsiElement, val incompleteCode: Boolean) :
+		ResolveProcessor(name) {
+	constructor(ref: CovSymbolRef, incompleteCode: Boolean) : this(ref.canonicalText, ref.element, incompleteCode)
+
+	var isFilter = true
+	private val processedElements = hashSetOf<PsiElement>()
+	private fun addCandidate(symbol: CovSymbol) = addCandidate(PsiElementResolveResult(symbol, true))
+	override fun <T : Any?> getHint(hintKey: Key<T>): T? = null
+	override fun execute(element: PsiElement, resolveState: ResolveState) =
+			if (element is CovSymbol && element !in processedElements) {
+				val accessible = name == element.text
+				if ((!isFilter || accessible) && ((element as? StubBasedPsiElement<*>)?.stub != null || !PsiTreeUtil.hasErrorElements(element)))
+					addCandidate(element)
+				processedElements.add(element)
+				!(isFilter and accessible) // || (place as PsiReference).element is LuaGlobal
+			} else true
 }
--- src/org/covscript/lang/psi/impl/cov-psi-mixins.kt
+++ src/org/covscript/lang/psi/impl/cov-psi-mixins.kt
@@ -2,14 +2,39 @@ package org.covscript.lang.psi.impl
 
 import com.intellij.extapi.psi.ASTWrapperPsiElement
 import com.intellij.lang.ASTNode
-import com.intellij.psi.PsiReference
+import com.intellij.psi.*
+import org.covscript.lang.CovLanguage
 import org.covscript.lang.psi.*
 
-abstract class CovVariableDeclarationMixin(node: ASTNode) : CovVariableDeclaration, ASTWrapperPsiElement(node) {
-	override fun getReferences(): Array<PsiReference> = super.getReferences() // TODO
+abstract class CovVariableDeclarationMixin(node: ASTNode) :
+		CovVariableDeclaration,
+		ASTWrapperPsiElement(node),
+		PsiNameIdentifierOwner {
+	private var references: Array<PsiReference>? = null
+	override fun getNameIdentifier() = symbol
+	override fun setName(name: String) = PsiFileFactory
+			.getInstance(project)
+			.createFileFromText(CovLanguage, name)
+			.firstChild
+
+	override fun getReferences(): Array<PsiReference> = references ?: SyntaxTraverser
+			.psiTraverser(parent.parent)
+			.filter { it is CovSymbol && it.text == symbol.text }
+			.mapNotNull { it.reference }
+			.toList()
+			.also(::println)
+			.toTypedArray()
+			.also { references = it }
+
+	override fun subtreeChanged() {
+		references = null
+		super.subtreeChanged()
+	}
 }
 
-abstract class CovSymbolMixin(node: ASTNode) : CovSymbol, ASTWrapperPsiElement(node) {
+abstract class CovSymbolMixin(node: ASTNode) :
+		CovSymbol,
+		ASTWrapperPsiElement(node) {
 	private val refCache by lazy { CovSymbolRef(this) }
-	override fun getReference() = refCache
+	override fun getReference() = if (parent is CovVariableDeclaration) null else refCache
 }
