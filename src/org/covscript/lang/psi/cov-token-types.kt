13ad68d7601c91165c267db6a181a70de5f0798d
ice1000 <ice1000kotlin@foxmail.com>
2018-01-22 17:45:48 +0800

    [ resolving ] Interesting

--- src/org/covscript/lang/psi/cov-token-types.kt
+++ src/org/covscript/lang/psi/cov-token-types.kt
@@ -1,5 +1,8 @@
 package org.covscript.lang.psi
 
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+import com.intellij.psi.PsiFileFactory
 import com.intellij.psi.tree.IElementType
 import com.intellij.psi.tree.TokenSet
 import org.covscript.lang.CovLanguage
@@ -9,5 +12,9 @@ class CovTokenType(debugName: String) : IElementType(debugName, CovLanguage) {
 		@JvmField val COMMENTS = TokenSet.create(CovTypes.COMMENT)
 		@JvmField val STRINGS = TokenSet.create(CovTypes.STR, CovTypes.CHAR)
 		@JvmField val CONCATENATABLE_TOKENS = TokenSet.orSet(COMMENTS, STRINGS)
+		fun fromText(name: String, project: Project): PsiElement = PsiFileFactory
+				.getInstance(project)
+				.createFileFromText(CovLanguage, name)
+				.firstChild
 	}
 }
--- src/org/covscript/lang/psi/impl/cov-psi-mixins.kt
+++ src/org/covscript/lang/psi/impl/cov-psi-mixins.kt
@@ -5,24 +5,19 @@ import com.intellij.lang.ASTNode
 import com.intellij.psi.*
 import com.intellij.psi.scope.PsiScopeProcessor
 import com.intellij.psi.util.PsiTreeUtil
-import org.covscript.lang.CovLanguage
 import org.covscript.lang.psi.*
 
-abstract class CovVariableDeclarationMixin(node: ASTNode) :
-		CovVariableDeclaration,
-		ASTWrapperPsiElement(node),
-		PsiNameIdentifierOwner {
-	private var references: Array<PsiReference>? = null
+abstract class CovVariableDeclarationMixin(node: ASTNode) : CovVariableDeclaration, TrivialDeclaration(node) {
 	override fun getNameIdentifier() = symbol
-	override fun setName(name: String): PsiElement = PsiFileFactory
-			.getInstance(project)
-			.createFileFromText(CovLanguage, name)
-			.firstChild
-			.let(::replace)
+}
 
+abstract class TrivialDeclaration(node: ASTNode) : ASTWrapperPsiElement(node), PsiNameIdentifierOwner {
+	private var references: Array<PsiReference>? = null
+	override fun setName(name: String): PsiElement = CovTokenType.fromText(name, project).let(::replace)
+	abstract override fun getNameIdentifier(): PsiElement
 	override fun getReferences(): Array<PsiReference> = references ?: SyntaxTraverser
 			.psiTraverser(parent.parent)
-			.filter { it is CovSymbol && it.text == symbol.text }
+			.filter { it is CovSymbol && it.text == nameIdentifier.text }
 			.mapNotNull(PsiElement::getReference)
 			.toList()
 			.toTypedArray()
@@ -33,12 +28,17 @@ abstract class CovVariableDeclarationMixin(node: ASTNode) :
 			substitutor: ResolveState,
 			lastParent: PsiElement?,
 			place: PsiElement) =
-			PsiTreeUtil.isAncestor(this, place, true) or processor.execute(symbol, substitutor)
+			PsiTreeUtil.isAncestor(this, place, true) ||
+					processor.execute(nameIdentifier, substitutor)
 
 	override fun subtreeChanged() {
 		references = null
 		super.subtreeChanged()
 	}
+}
+
+abstract class CovFunctionDeclarationMixin(node: ASTNode) : CovFunctionDeclaration, TrivialDeclaration(node) {
+	override fun getNameIdentifier() = symbol
 }
 
 abstract class CovSymbolMixin(node: ASTNode) :
