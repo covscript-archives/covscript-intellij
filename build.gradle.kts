de18ba1147c573ea1ea006db6951f8e82e9fe678
ice1000 <ice1000kotlin@foxmail.com>
2018-03-04 05:42:07 +0800

    [ release ] 1.8 is available.

--- build.gradle.kts
+++ build.gradle.kts
@@ -139,7 +139,7 @@ repositories {
 }
 
 dependencies {
-	compile(kotlin("stdlib", kotlinVersion))
+	compileOnly(kotlin("stdlib", kotlinVersion))
 	compile(files(Paths.get("lib", "org.eclipse.egit.github.core-2.1.5.jar")))
 	testCompile("junit", "junit", "4.12")
 }
