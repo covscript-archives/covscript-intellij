595604c40dbe45630322ee6bea03ebfed132462a
ice1000 <ice1000@sbrella.com>
2018-03-12 14:14:37 +0800

    [ jdk8 ] Add

--- build.gradle.kts
+++ build.gradle.kts
@@ -138,7 +138,7 @@ repositories {
 }
 
 dependencies {
-	compileOnly(kotlin("stdlib", kotlinVersion))
+	compileOnly(kotlin("stdlib-jdk8", kotlinVersion))
 	compile(kotlin("stdlib-jdk8", kotlinVersion).toString()) {
 		exclude(module = "kotlin-runtime")
 		exclude(module = "kotlin-reflect")
