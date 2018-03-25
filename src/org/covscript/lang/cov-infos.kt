4ca8b064b8e49d4a1b66c5d9ee33deb3f9cae317
ice1000 <ice1000kotlin@foxmail.com>
2018-03-26 01:02:54 +0800

    [ fix ] Remove bad code

--- src/org/covscript/lang/cov-infos.kt
+++ src/org/covscript/lang/cov-infos.kt
@@ -6,10 +6,9 @@ import com.intellij.codeInsight.template.impl.DefaultLiveTemplatesProvider
 import com.intellij.extapi.psi.PsiFileBase
 import com.intellij.openapi.fileTypes.*
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.psi.*
-import com.intellij.psi.scope.PsiScopeProcessor
+import com.intellij.psi.FileViewProvider
+import com.intellij.psi.PsiFile
 import icons.CovIcons
-import org.covscript.lang.psi.impl.processDeclTrivial
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 import java.util.*
@@ -30,9 +29,6 @@ object CovFileType : LanguageFileType(CovLanguage.INSTANCE) {
 
 class CovFile(viewProvider: FileViewProvider) : PsiFileBase(viewProvider, CovLanguage.INSTANCE) {
 	override fun getFileType() = CovFileType
-	override fun processDeclarations(
-			processor: PsiScopeProcessor, state: ResolveState, lastParent: PsiElement?, place: PsiElement) =
-			processDeclTrivial(processor, state, lastParent, place)
 }
 
 class CovFileTypeFactory : FileTypeFactory() {
