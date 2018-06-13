d68af247cec3328e348adac0958ad4f205c94ad8
ice1000 <ice1000@sbrella.com>
2018-06-13 21:22:50 +0800

    [ version ] Change version number, add new release

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ val commitHash by lazy {
 
 val isCI = !System.getenv("CI").isNullOrBlank()
 
-val pluginComingVersion = "1.9.0"
+val pluginComingVersion = "1.9.1"
 val pluginVersion = if (isCI) "$pluginComingVersion-$commitHash" else pluginComingVersion
 val packageName = "org.covscript"
 val kotlinVersion = "1.2.41"
