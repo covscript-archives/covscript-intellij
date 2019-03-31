7b3233c2ca4764f4c04c5e33492cece95bc8432f
ice1000 <ice1000kotlin@foxmail.com>
2019-03-30 21:34:38 -0400

    [ bump ] Update version to 2.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ val commitHash by lazy {
 
 val isCI = !System.getenv("CI").isNullOrBlank()
 
-val pluginComingVersion = "2.0.0"
+val pluginComingVersion = "2.0.1"
 val pluginVersion = if (isCI) "$pluginComingVersion-$commitHash" else pluginComingVersion
 val packageName = "org.covscript"
 
